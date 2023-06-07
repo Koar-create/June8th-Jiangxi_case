16e9aa12766b7c5f598a11a7a6bced5a762923ec
Koar-create <yimu01439@gmail.com>
2023-06-08 02:19:29 +0800

    Initial commit.

---
+++ process_repetitive_line.py
@@ -0,0 +1,14 @@
+import os
+
+def remove_duplicates(input_file, output_file):
+    lines_seen = set()
+    with open(output_file, 'w') as out_file:
+        with open(input_file, 'r') as in_file:
+            for line in in_file:
+                if line not in lines_seen:
+                    out_file.write(line)
+                    lines_seen.add(line)
+
+input_file = os.path.join(os.getcwd(), 'fnl_var_short_name.txt')
+output_file = os.path.join(os.getcwd(), '1_fnl_var_short_name.txt')
+remove_duplicates(input_file, output_file)

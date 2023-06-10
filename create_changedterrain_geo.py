d468bd22d3787f1f7c90f761435ba8e427b8b26a
Koar-create <yimu01439@gmail.com>
2023-06-10 21:27:58 +0800

    properly change it.

--- create_changedterrain_geo.py
+++ create_changedterrain_geo.py
@@ -1,28 +1,44 @@
+import numpy as np
 import netCDF4 as nc
 import xarray as xr
 
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import os
+
 # 打开.nc文件（以读写模式）
 file_path = '/home/koyomi/Downloads/geo_em.d00.nc'  # 将路径替换为你的文件路径
-dataset = nc.Dataset(file_path, 'r+')
+
 nc_data = xr.open_dataset(file_path)
+lon = nc_data['XLONG_M'][0,:,:][0,:].data
+lat = nc_data['XLAT_M'][0,:,:][:,0].data
+lon_idx = np.where((lon>=116)&(lon<=119))
+lat_idx = np.where((lat>=28)&(lat<=30))
+# hgt[0,lat_idx[0],lon_idx[0]] = 0.
+nc_data.close()
 
 # 获取要修改的变量
-lon = nc_data['XLONG_M']
-lat = nc_data['XLAT_M']
-hgt = nc_data['HGT_M']
-hgt1 = nc_data['HGT_M'].sel(lat=(28,30), lon=(116,119))
-print(hgt.shape, hgt1.shape)
-# variable = dataset.variables[variable_name]
+dataset = nc.Dataset(file_path, 'r+')
+hgt = dataset.variables['HGT_M']
+# hgt[0, lat_idx[0], lon_idx[0]] = 0.
 
-# # 获取变量的当前数据
-# data = variable[:]
+X, Y = np.meshgrid(lon, lat)
+standard_parallels = (30.0, 60.0)
+central_longitude = 115.0
+projection = ccrs.LambertConformal(central_latitude=standard_parallels[0],
+                                   central_longitude=central_longitude,
+                                   standard_parallels=standard_parallels)
 
-# # 添加一个数字到每个元素
-# number_to_add = 10
-# modified_data = data + number_to_add
+fig, ax = plt.subplots(figsize=(8, 6), subplot_kw={'projection': projection})
 
-# # 将修改后的数据写回变量
-# variable[:] = modified_data
+plt.pcolormesh(X, Y, hgt[0,:,:], shading='auto', transform=ccrs.PlateCarree(), cmap='jet')
+
+ax.gridlines(draw_labels=True, alpha=0.5)
+ax.set_title('HGT_M')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+
+plt.show()
 
 # 关闭数据集，保存修改
 dataset.close()

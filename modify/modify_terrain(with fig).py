a4673c6426ba14295050b49a99d37c25d5e98945
Koyomi <yimu01439@gmail.com>
2023-06-16 02:42:54 +0800

    add '__*' into .gitignore, upgrade modify_terrain(with fig).py

--- modify/modify_terrain(with fig).py
+++ modify/modify_terrain(with fig).py
@@ -1,99 +1,76 @@
-import numpy as np, matplotlib as mpl, matplotlib.pyplot as plt
-import cartopy.crs as ccrs, cartopy.feature as cfeature
-from cartopy.mpl.ticker import (LongitudeFormatter, LatitudeFormatter)
-import os, netCDF4 as nc, xarray as xr
+## 0.1
+import numpy as np
+import matplotlib as mpl, matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from netCDF4 import Dataset
+import os, sys
 
-def add_cfeature(ax, linewidth=0.8):
-    china_provinces = cfeature.NaturalEarthFeature(
-        category='cultural', name='admin_1_states_provinces_lines',
-        scale='50m',         facecolor='none',
-        edgecolor='black',   linewidth=linewidth)
-    ax.add_feature(cfeature.BORDERS, linewidth=linewidth)
-    ax.add_feature(cfeature.COASTLINE, linewidth=linewidth)
-    # ax.add_feature(cfeature.STATES, linewidth=linewidth)  # ruler
-    ax.add_feature(china_provinces)
+## 0.2
+sys.path.append("D:\\Repositories\\June8th-Jiangxi_case")
+sys.path.append("D:\\Repositories\\June8th-Jiangxi_case\\obtain")
+from add_figure import add_cfeature, add_gridlines, add_colorbar
 
-def add_gridlines(ax, linewidth=1, color='gray', linestyle='--', alpha=0.5, fontsize=18, fontname='Consolas'):
-    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, x_inline=False, y_inline=False, \
-        linewidth=linewidth, color=color, linestyle=linestyle, alpha=alpha)
-    gl.xformatter = LongitudeFormatter()
-    gl.yformatter = LatitudeFormatter()
-    gl.xlabel_style = {'fontsize': fontsize, 'fontname': fontname}
-    gl.ylabel_style = {'fontsize': fontsize, 'fontname': fontname}
-    gl.top_labels    = False
-    gl.bottom_labels = True
-    gl.left_labels = True
-    gl.right_labels = False
-    gl.rotate_labels = False
+## 0.3 function
+# no function
 
-def add_colorbar(im, ax, cax, title='units: m', levels=np.linspace(0, 1000, 11), extend='max', orientation='vertical', labelsize=18, fontsize=26, fontname='Consolas', x_axis=0.95, y_axis=0.5):
-    cbar = plt.colorbar(im, ax=ax, cax=cax, extend=extend, orientation=orientation)
-    cbar.ax.tick_params(labelsize=labelsize)
-    cbar.set_ticks(levels)
-    if orientation == 'vertical':
-        fig.text(x_axis, y_axis, title, va='center', ha='center', rotation='vertical', fontdict={'size': fontsize, 'family': fontname})  # cbar title
-
-# basic setting
+## 0.4 basic settings
 vmin, vmax = 0, 1000
 levels = np.linspace(vmin, vmax, 11)
 crop_box = [116, 120, 28, 31]
 cmap = mpl.cm.gray_r
 
+## 0.5 dim
 dim1 = 'd01'  # 'd01' or 'd02'
 mode = 'modified'  # 'modified' or 'unmodified'
 res  = '9-km'
 
-# title
+## 0.6
+# string
 title       = f"{mode} terrain"
-# file_path = '/home/username/Downloads/geo_em.d01(modified).nc'  # path in linux system
-file_path   = f"D:\\Repositories\\June8th-Jiangxi_case"
+file_path   = f"D:\\Repositories\\June8th-Jiangxi_case\\geo_em.d0"  # or '/home/username/Downloads/geo_em.d01(modified).nc'  # path in linux system
 save_path   = f"D:\\Repositories\\June11th-Jiangxi_case_figures\\fig_terrain"
-
-## 1. open .nc file (in read-write mode), read necessary variables  ##
-dataset = nc.Dataset(os.path.join(file_path, f"geo_em.{dim1}.{mode}_{res}.nc"), 'r+')
-lon = dataset.variables['XLONG_M'][0, 0, :].data
-lat = dataset.variables['XLAT_M'][ 0, :, 0].data
-hgt = dataset.variables['HGT_M']
-lon_idx = np.where((lon>=crop_box[0])&(lon<=crop_box[1]))
-lat_idx = np.where((lat>=crop_box[2])&(lat<=crop_box[3]))
-# hgt[0, lon_idx[0], lat_idx[0]] = 0.
-''''''''''''''''''''''''''' ↓ test part ↓ '''''''''''''''''''''''''''
-
-## 2. paint figure ##
-# create figure and axes
-LON, LAT = np.meshgrid(lon, lat)
+# other
 central_latitude = 26 if dim1 == 'd01' else 28.4
+cax_box = [0.83,0.12,0.03,0.75] if dim1 == 'd01' else \
+          [0.83,0.15,0.03,0.71]
+
+## 1. & 2. open .nc file (in read-write mode), read necessary variables  ##
+dataset = Dataset(os.path.join(file_path, f"geo_em.{dim1}.{mode}_{res}.nc"), 'r+')
+lon,     lat, hgt = dataset.variables['XLONG_M'][0, 0, :].data, dataset.variables['XLAT_M'][0, :, 0].data, dataset.variables['HGT_M']
+lon_idx, lat_idx  = np.where((lon>=crop_box[0])&(lon<=crop_box[1]))[0], np.where((lat>=crop_box[2])&(lat<=crop_box[3]))[0]
+# hgt[0, lon_idx, lat_idx] = 0.
+''''''''''''''''''''''''''' ↓ test part ↓ '''''''''''''''''''''''''''
+## 3.1 create figure and axes
 fig = plt.figure(figsize=(10, 8))
 ax  = plt.axes(projection=ccrs.LambertConformal(central_longitude=116, central_latitude=central_latitude, standard_parallels=(30.0, 60.0)))
-# restrict x-y axis
+
+## 3.2 add borders, coastlines, and states
+add_cfeature(ax, linewidth=0.8)
+
+## 3.3 add grid
+add_gridlines(ax, linewidth=1, color='gray', linestyle='--', alpha=0.5, fontsize=18, fontname='Consolas')
+
+# 3.4 restrict x-y extent
+LON, LAT = np.meshgrid(lon, lat)
 box = [np.min(LON)+1.41, np.max(LON)-1.51, np.min(LAT)+0.10, np.max(LAT)-0.21] if dim1 == 'd01' else \
       [np.min(LON)+0.12, np.max(LON)-0.20, np.min(LAT)+0.00, np.max(LAT)-0.10]
 ax.set_extent(box, crs=ccrs.PlateCarree())
-# add borders, coastlines, and states
-add_cfeature(ax, linewidth=0.8)
-# add grid
-add_gridlines(ax, linewidth=1, color='gray', linestyle='--', alpha=0.5, fontsize=18, fontname='Consolas')
-# im
+
+## 3.5 create image
 im = ax.contourf(LON, LAT, hgt[0, :, :].data, extend='max', levels=levels, cmap=cmap, transform=ccrs.PlateCarree())
-# add colorbar
-cax = fig.add_axes([0.83,0.12,0.03,0.75]) if dim1 == 'd01' else \
-      fig.add_axes([0.83,0.15,0.03,0.71])
-add_colorbar(im, ax, cax, title='units: m', levels=levels, extend='max', orientation='vertical', labelsize=18, fontsize=26, fontname='Consolas', x_axis=0.95, y_axis=0.5)
-# set up title
+
+## 3.6 add colorbar
+add_colorbar(im, ax, fig.add_axes(cax_box), fig, title='units: m', levels=levels, extend='max', orientation='vertical', labelsize=18, fontsize=26, fontname='Consolas', x_axis=0.95, y_axis=0.5)
+
+## 3.7 set up title
 ax.set_title(title, fontdict={'size': 28, 'family': 'Arial', 'weight': 'bold'})
-# plt.show()  # don't show.
-# adjust axes position
+
+## 3.8 adjust axes position
 plt.subplots_adjust(right=0.79)
 
-## 3. save picfure ##
-if os.path.exists(save_path):
-    print(f"directory '{save_path}' already exists.")
-else:
-    os.makedirs(save_path)
-    print(f"Successfully create directory '{save_path}'!")
+## 3.9 show, save and close
+# plt.show()  # don't show.
+if not os.path.exists(save_path):
+    os.makedirs(save_path), print(f"Successfully create directory '{save_path}'!")
 plt.savefig(os.path.join(save_path, f"{mode}-terrain_{res}_{dim1}.png"), dpi=300)
-
-## 4. ending ##
-plt.close(fig)
-print("DONE.")
-dataset.close()
+plt.close(fig), print("DONE."), dataset.close()
